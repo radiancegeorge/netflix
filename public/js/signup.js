b48f3deb87925ff151558ab58569293f22a2ebdc
preshnwoko <preshnwoko@gmail.com>
2020-07-17 14:39:00 +0100

    signup mobile waiting validation

---
+++ public/js/signup.js
@@ -0,0 +1,9 @@
+document.querySelector(".signup").addEventListener("onSubmit", function(e){
+    e.preventDefault()
+}
+)
+let submit = Document.querySelector(".submit")
+
+submit.addEventListener("click", function(e){
+    e.preventDefault()
+} )
