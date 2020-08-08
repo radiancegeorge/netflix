139bcaf5b3b85a061ec169331968f1033a76983a
radiancegeorge <radiancegeorge@outlook.com>
2020-08-08 12:38:43 +0100

    all set about signing out

--- public/src/js/dashboard_setup.js
+++ public/src/js/dashboard_setup.js
@@ -1,13 +1,20 @@
 // const cancel = document.querySelector('.cancel'),
 //     logout = document.querySelector('.logout'),
    const logoutPrompt = document.querySelector('.prompt');
+   const reminder = document.querySelector('.reminder')
 
     window.addEventListener('click', (e)=>{
+        console.log(e.target)
         if(e.target.id === 'logout'){
             logoutPrompt.classList.toggle('hide');
         }else if(e.target.className === 'cancel'){
             logoutPrompt.classList.toggle('hide');
+        }else if(e.target.className === 'cancel-reminder'){
+            reminder.style.display = 'none'
         }
-    })
-const date = new Date();
-console.log(date.getTime())
+    });
+    setTimeout(() => {
+        reminder.style.display = 'none'
+    }, 20000);
+// const date = new Date();
+// console.log(date.getTime())
